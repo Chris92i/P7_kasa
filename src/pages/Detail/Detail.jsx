994fde34eb7670e73a78ea3e323ca0336bfbb541
Chris92i <chris.p.teixeira@gmail.com>
2023-11-14 23:04:37 +0100

    debut page detail recuperation des donnees

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,37 +1,41 @@
 // pour récupérer l'identifiant dans l'url il faut utiliser use params
 import {useParams, Navigate } from "react-router-dom";
-import { useEffect, useState} from "react"
+import { useCallback, useEffect, useState} from "react"
 import Tag from "../../components/Tag/Tag";
+import './style.scss'
+import Appart from "../../components/Appart/Appart";
+//import Appart from "../../components/Appart/Appart"
+
+
 
 
 function Detail(){
     const { id } = useParams();
     console.log(id)
-    const [data, setData] = useState([])
-    const getData = async () => {
-        const response = await fetch("logements.json", {
+    const [data, setData] = useState({})
+    const getData =useCallback( async () => {
+        const response = await fetch("/logements.json", {
             headers: {
                 "Content-Type": "application/json",
                 Accept: "application/json",
             },
         });
         const cards = await response.json();
-        const apartment = cards.find(apartment => apartment.id === id)
+        console.log(cards)
+        const apartment = cards.find(apartment => apartment.id == id)
         setData(apartment);
-    };
+        if (typeof apartment.id === "undefined") return <Navigate to="*" />
+    }, [setData, id]);
     useEffect(() => {
         getData();
-    }, []);
+    }, [getData]);
 
-    if (!data) return <Navigate to="*" />;
 
+    console.log('data is:' , data ); 
     return (
         <>
         hello Detail
         <h1>{data.title}</h1>
-        <div>
-            {data.description}
-        </div>
 
         <Tag/>
         </>
