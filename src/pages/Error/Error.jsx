3230bd49b1e9bd8a3fb06a40d2a2d9a7819e1f04
Chris92i <chris.p.teixeira@gmail.com>
2023-11-28 14:33:57 +0100

    avancement sur le style et sur la page home

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,11 +1,14 @@
 import './index.scss'
+import { Link } from 'react-router-dom'
 
 function Error(){
     return (
         <>
+            <div className='erreur_container'>
             <p className='erreur'>404</p>
             <p className='erreur_oups'>Oups! La page que vous demandez n'existe pas.</p>
-            <p className='erreur_oups_back'>Retourner sur la page d’accueil</p>
+            <Link className='erreur_oups_back' to="/">Retourner sur la page d’accueil</Link>
+            </div>
         </>
     )
 
